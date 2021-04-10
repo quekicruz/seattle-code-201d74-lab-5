7cf181b3143f25cf8db2014ad316af3130ce0216
Quentin Cruz <napu0515@gmail.com>
2021-04-10 10:18:12 -0700

    solvec4

--- starterCode/app.js
+++ starterCode/app.js
@@ -58,7 +58,6 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
   return sPArray;
 }
 
-
 // Here is the test for sumAndMultiply(); uncomment it to run it
 testSumAndMultiply(4, 7, 5);
 
@@ -76,11 +75,15 @@ let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
+let arraySumOne = sum(sumArr[0],sumArr[1][0]);
+let arraySumTwo = sum(arraySumOne, sumArr[2][0]);
+let wholeArray =  [arraySumTwo, sumArr[0]+','+sumArr[1]+','+sumArr[2]+' was passed in as an array of numbers, and '+arraySumTwo+' is their sum.'];
+return wholeArray
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
